80cb5dcf7981584ceb4ce6f952b1ee1ca7b3b857
Chris Zielinski <ckz8780@gmail.com>
2018-05-27 21:11:36 +0000

    Added for loop example

--- run.py
+++ run.py
@@ -5,19 +5,36 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', page_heading="Home")
+    return render_template(
+                    'index.html',
+                    page_heading="Home",
+                    template_var="This (home) heading was generated with a template variable (see run.py)",
+                    list_of_numbers=[1, 2, 3, 4,5]
+                )
     
 @app.route('/about')
 def about():
-    return render_template('about.html', page_heading="About")
+    return render_template(
+                    'about.html',
+                    page_heading="About",
+                    template_var="This (about) heading was generated with a template variable (see run.py)"
+                )
     
 @app.route('/contact')
 def contact():
-    return render_template('contact.html', page_heading="Contact")
+    return render_template(
+                    'contact.html',
+                    page_heading="Contact",
+                    template_var="This (contact) heading was generated with a template variable (see run.py)"
+                )
     
 @app.route('/careers')
 def careers():
-    return render_template('careers.html', page_heading="Careers")
+    return render_template(
+                    'careers.html',
+                    page_heading="Careers",
+                    template_var="This (careers) heading was generated with a template variable (see run.py)"
+                )
     
 if __name__ == '__main__':
     app.run(
